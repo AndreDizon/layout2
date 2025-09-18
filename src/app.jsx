96a21863ba2ec5f47eb7ca87b22a10c6c725b390
AndreDizon <cardizon.student@ua.edu.ph>
2025-09-18 20:21:42 +0800

    second commit

--- src/app.jsx
+++ src/app.jsx
@@ -1,38 +1,83 @@
-import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import viteLogo from '/vite.svg'
 import './app.css'
 
-export function App() {
-  const [count, setCount] = useState(0)
+function TopLeftContent() {
+  return <div className="header-container"></div>;
+}
 
+function TopCenterContent() {
   return (
-    <>
-      <div class="main-container">
-        <div class="top-row">
-            <div class="top-left"></div>
-            <div class="top-center">
-                <div class="top-center-box"></div>
-                <div class="top-center-box"></div>
-            </div>
-            <div class="top-right"></div>
-        </div>
-        <div class="middle-row">
-            <div class="middle-left"></div>
-            <div class="middle-right">
-                <div class="middle-right-box-up"></div>
-                <div class="middle-right-box-down"></div>
-            </div>
-        </div>
-        <div class="bottom-row">
-            <div class="bottom-left"></div>
-            <div class="bottom-center">
-                <div class="bottom-center-box"></div>
-                <div class="bottom-center-box"></div>
-            </div>
-            <div class="bottom-right"></div>
-        </div>
+    <div className="header-container2">
+      <TopCenterBox name="Andre" />
+      <TopCenterBox name="Dizon" />
     </div>
-    </>
-  )
+  );
+}
+
+function TopCenterBox(props) {
+  return <div className="header-container2-content">{props.name}</div>;
+}
+
+function TopSection() {
+  return (
+    <div className="header">
+      <TopLeftContent />
+      <TopCenterContent />
+      <TopLeftContent />
+    </div>
+  );
+}
+
+function MiddleLeftContent() {
+  return <div className="body-container"></div>;
+}
+
+function MiddleRightBox() {
+  return <div className="body-container2-content"></div>;
+}
+
+function MiddleRightContent() {
+  return (
+    <div className="body-container2">
+      <MiddleRightBox />
+      <MiddleRightBox />
+    </div>
+  );
+}
+
+function MiddleSection() {
+  return (
+    <div className="body">
+      <MiddleLeftContent />
+      <MiddleRightContent />
+    </div>
+  );
+}
+
+function BottomLeftContent() {
+  return <div className="footer-container1"></div>;
+}
+
+function BottomCenterBox(props) {
+  return <div className="footer-container2">{props.text}</div>;
+}
+
+function BottomSection() {
+  return (
+    <div className="footer">
+      <BottomLeftContent />
+      <BottomCenterBox text="C-PCIT9" />
+      <BottomCenterBox text="IT3A" />
+      <BottomLeftContent />
+    </div>
+  );
+}
+
+export function App() {
+  return (
+    <div className="container">
+      <TopSection />
+      <MiddleSection />
+      <BottomSection />
+    </div>
+  );
 }
